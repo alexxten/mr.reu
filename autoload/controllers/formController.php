3b2f127798560f09a7c95a2c77b232835cdc3917
alexxten <1399selena@gmail.com>
2018-01-08 18:04:35 +0300

    remove array from input photo

--- autoload/controllers/formController.php
+++ autoload/controllers/formController.php
@@ -25,18 +25,18 @@ class formController{
 
        // $data[photo] = $files;
 
-        if ( mb_strlen($data[fio])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле ФИО ") ;}
-        elseif ( mb_strlen($data[kurs])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Курс ") ;}
-        elseif ( mb_strlen($data[fakultet])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Факультет ") ;}
-        elseif ( mb_strlen($data[phone])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Телефон ") ;}
-        elseif ( mb_strlen($data[vklink])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Ссылка вк ") ;}
-        elseif ( mb_strlen($data[height])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Рост ") ;}
-        elseif ( mb_strlen($data[hobbies])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Чем занимался (занимается), хобби ") ;}
-        elseif ( mb_strlen($data[mr_reu_2018])<1 ) {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Что нужно, чтобы стать Мистером РЭУ-2018? ") ;}
-        elseif ( mb_strlen($data[fio])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле ФИО  ") ;}
-        elseif ( mb_strlen($data[kurs])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Курс  ") ;}
-        elseif ( mb_strlen($data[fakultet])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Факультет  ") ;}
-        elseif ( mb_strlen($data[phone])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Телефон ") ;}
+        if ( mb_strlen($data[fio])<1 )                 {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле ФИО ") ;}
+        elseif ( mb_strlen($data[kurs])<1 )            {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Курс ") ;}
+        elseif ( mb_strlen($data[fakultet])<1 )        {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Факультет ") ;}
+        elseif ( mb_strlen($data[phone])<1 )           {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Телефон ") ;}
+        elseif ( mb_strlen($data[vklink])<1 )          {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Ссылка вк ") ;}
+        elseif ( mb_strlen($data[height])<1 )          {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Рост ") ;}
+        elseif ( mb_strlen($data[hobbies])<1 )         {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Чем занимался (занимается), хобби ") ;}
+        elseif ( mb_strlen($data[mr_reu_2018])<1 )     {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Что нужно, чтобы стать Мистером РЭУ-2018? ") ;}
+        elseif ( mb_strlen($data[fio])>256 )           {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле ФИО  ") ;}
+        elseif ( mb_strlen($data[kurs])>256 )          {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Курс  ") ;}
+        elseif ( mb_strlen($data[fakultet])>256 )      {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Факультет  ") ;}
+        elseif ( mb_strlen($data[phone])>256 )         {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Телефон ") ;}
         elseif ( mb_strlen($data[vklink])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Ссылка вк ") ;}
         elseif ( mb_strlen($data[height])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Рост ") ;}
         elseif ( mb_strlen($data[hobbies])>1024 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 1024 символов в поле Чем занимался (занимается), хобби ") ;}
