71c53ed87c61a72e0853552ccaada5e36e80fa60
alexxten <1399selena@gmail.com>
2018-01-08 23:11:32 +0300

    download files on server

--- autoload/models/formAction.php
+++ autoload/models/formAction.php
@@ -15,7 +15,21 @@ class formAction{
 
 	public static function doneSend($f3, $data) // Не забудь указать аргументы необходимые
 	{
-		/*$f3->get('DB')->exec('INSERT INTO `users`(`id`, `fio`, `kurs`, `fakultet`, `phone`, `vklink`, `height`, `hobbies`, `mr_reu_2018`, `photo`) VALUES (?,?,?,?,?,?,?,?,?,?)', 
+		$folderName = md5(microtime()); // Берет время в миллисекундах и хэширует MD5
+		$dir = "/home/s/slink21/mister_reu/public_html/ui/misters/$folderName/"; // В переменную dir записываем название нашей новой директории
+		mkdir($dir); // Создаём дирекорию
+
+		$jsonPathPhoto = array();
+		
+		foreach ($data[photo] as $key => &$value) 
+		{
+			$info = new SplFileInfo($value[name]);
+			move_uploaded_file($value[tmp_name], $dir."$key.".$info->getExtension());
+			$jsonPathPhoto[$key] = "ui/misters/$folderName/$key.".$info->getExtension();
+		}
+		unset($value);
+
+		$f3->get('DB')->exec('INSERT INTO `users`(`id`, `fio`, `kurs`, `fakultet`, `phone`, `vklink`, `height`, `hobbies`, `mr_reu_2018`, `photo`) VALUES (?,?,?,?,?,?,?,?,?,?)', 
 		array(
             1=> NULL, 
 			2=> $data[fio],
@@ -26,14 +40,13 @@ class formAction{
 			7=> $data[height],
 			8=> $data[hobbies],
 			9=> $data[mr_reu_2018],
-			10=> $data[photo]
-			     
+			10=> json_encode($jsonPathPhoto)                 // Передать json 	     
 		));
 		
         // Записать данные в БД
 
 		// Отправить на мыло
-		$to  = "1399selena@gmail.com";
+		/*$to  = "1399selena@gmail.com";
 		$subject = "Мистер РЭУ - $data[fio]";
 		$message = "<html><body>
 					<p>ФИО: $data[fio]</p>
