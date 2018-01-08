e8d381f7d711a32454d3340cce3c050f6bba3233
wanku-x <wanku@bk.ru>
2018-01-08 21:24:41 +0300

    Validate images

--- autoload/controllers/formController.php
+++ autoload/controllers/formController.php
@@ -3,7 +3,7 @@ namespace controllers;
 
 class formController{
 
-    private function reArrayFiles(&$file_post) {
+    private static function reArrayFiles(&$file_post) {
         $file_array = array();
         $file_count = count($file_post['name']);
         $file_keys = array_keys($file_post);
@@ -16,16 +16,21 @@ class formController{
     
         return $file_array;
     }
+
+    private static function checkMimeType(&$files) {
+        foreach($files as $photo) {
+            if (($photo[type]!='image/gif')&&($photo[type]!='image/jpeg')&&
+                ($photo[type]!='image/pjpeg')&&($photo[type]!='image/png')&&
+                ($photo[type]!='image/tiff'))
+            {
+                return FALSE;
+            }
+        }
+        return TRUE;
+    }
     
     public function sendData($f3)
 	{
-        // Так получать данные, присланные методом POST. Угадай, как получать данные методом GET :)
-      
-
-        // Так доводим строку до вида, который не навредит нам
-        // $name = trim(htmlspecialchars(stripslashes($name)));
-
-       
         $data = $f3->get('POST');
         $files = formController::reArrayFiles($f3->get('FILES.photo'));
 
@@ -40,22 +45,26 @@ class formController{
 
         $data[photo] = $files;
 
-        if ( mb_strlen($data[fio])<1 )                 {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле ФИО ") ;}
-        elseif ( mb_strlen($data[kurs])<1 )            {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Курс ") ;}
-        elseif ( mb_strlen($data[fakultet])<1 )        {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Факультет ") ;}
-        elseif ( mb_strlen($data[phone])<1 )           {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Телефон ") ;}
-        elseif ( mb_strlen($data[vklink])<1 )          {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Ссылка вк ") ;}
-        elseif ( mb_strlen($data[height])<1 )          {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Рост ") ;}
-        elseif ( mb_strlen($data[hobbies])<1 )         {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Чем занимался (занимается), хобби ") ;}
-        elseif ( mb_strlen($data[mr_reu_2018])<1 )     {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле  Что нужно, чтобы стать Мистером РЭУ-2018? ") ;}
-        elseif ( mb_strlen($data[fio])>256 )           {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле ФИО  ") ;}
-        elseif ( mb_strlen($data[kurs])>256 )          {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Курс  ") ;}
-        elseif ( mb_strlen($data[fakultet])>256 )      {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Факультет  ") ;}
-        elseif ( mb_strlen($data[phone])>256 )         {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Телефон ") ;}
-        elseif ( mb_strlen($data[vklink])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Ссылка вк ") ;}
-        elseif ( mb_strlen($data[height])>256 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Рост ") ;}
-        elseif ( mb_strlen($data[hobbies])>1024 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 1024 символов в поле Чем занимался (занимается), хобби ") ;}
-        elseif ( mb_strlen($data[mr_reu_2018])>1024 ) {\models\formAction::errorSend($f3, 1, "Вы ввели больше 1024 символов в поле Что нужно, чтобы стать Мистером РЭУ-2018? ") ;}
+        if     (mb_strlen($data[fio]) < 1)              {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле ФИО") ;}
+        elseif (mb_strlen($data[kurs]) < 1)             {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Курс") ;}
+        elseif (mb_strlen($data[fakultet]) < 1)         {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Факультет") ;}
+        elseif (mb_strlen($data[phone]) < 1)            {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Телефон") ;}
+        elseif (mb_strlen($data[vklink]) < 1)           {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Ссылка ВК") ;}
+        elseif (mb_strlen($data[height]) < 1)           {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Рост") ;}
+        elseif (mb_strlen($data[hobbies]) < 1)          {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Чем занимался (занимается), хобби") ;}
+        elseif (mb_strlen($data[mr_reu_2018]) < 1)      {\models\formAction::errorSend($f3, 1, "Вы не заполнили поле Что нужно, чтобы стать Мистером РЭУ-2018?") ;}
+        elseif (mb_strlen($data[fio]) > 256)            {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле ФИО") ;}
+        elseif (mb_strlen($data[kurs]) > 256)           {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Курс") ;}
+        elseif (mb_strlen($data[fakultet]) > 256)       {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Факультет") ;}
+        elseif (mb_strlen($data[phone]) > 256)          {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Телефон") ;}
+        elseif (mb_strlen($data[vklink]) > 256)         {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Ссылка ВК") ;}
+        elseif (mb_strlen($data[height]) > 256)         {\models\formAction::errorSend($f3, 1, "Вы ввели больше 256 символов в поле Рост") ;}
+        elseif (mb_strlen($data[hobbies]) > 1024)       {\models\formAction::errorSend($f3, 1, "Вы ввели больше 1024 символов в поле Чем занимался (занимается), хобби") ;}
+        elseif (mb_strlen($data[mr_reu_2018]) > 1024)   {\models\formAction::errorSend($f3, 1, "Вы ввели больше 1024 символов в поле Что нужно, чтобы стать Мистером РЭУ-2018?") ;}
+        elseif (count($data[photo]) < 3)                {\models\formAction::errorSend($f3, 1, "Вы загрузили меньше 3-х фотографий") ;}
+        elseif (count($data[photo]) > 5)                {\models\formAction::errorSend($f3, 1, "Вы загрузили больше 5-и фотографий") ;}
+        elseif (formController::checkMimeType($data[photo])) {\models\formAction::errorSend($f3, 1, "Допустимые расширения изображений: jpeg, png, gif, tiff") ;}
+
         //else {\models\formAction::doneSend($f3, $data);}
         
         echo "<pre>";
