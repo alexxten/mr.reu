5934de0c2f74cb281d669e0365299965bc8cddf0
alexxten <1399selena@gmail.com>
2018-01-10 20:57:41 +0300

    change github.php

--- github.php
+++ github.php
@@ -1,4 +1,5 @@
 <?php
     echo exec('git reset --hard HEAD && git pull');
-    echo exec('git clone https://github.com/leralins/Mr.REA.git && cp Mr.REA/build/*.html ui && cp -R Mr.REA/build/ui/* ui && rm -rf Mr.REA');
- 
+    $output = [];
+    exec('git clone https://github.com/leralins/Mr.REA.git && cp Mr.REA/build/*.html ui && cp -R Mr.REA/build/ui/* ui && rm -rf Mr.REA', $output);
+    print_r(output);
