c94ac64aed1ecc6b540182e2dc566ca115933b7b
alexxten <1399selena@gmail.com>
2018-01-10 20:38:48 +0300

    one more echo exec

--- github.php
+++ github.php
@@ -1,4 +1,4 @@
 <?php
-    exec('git reset --hard HEAD && git pull');
+    echo exec('git reset --hard HEAD && git pull');
     echo exec('git clone https://github.com/leralins/Mr.REA.git && cp Mr.REA/build/*.html ui && cp -R Mr.REA/build/ui/* ui && rm -rf Mr.REA');
  
