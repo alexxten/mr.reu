1dc0931f23a79c9e0f107880b82032fa5ef47257
alexxten <1399selena@gmail.com>
2018-01-10 19:01:32 +0300

    fixbug github.php

--- github.php
+++ github.php
@@ -1,4 +1,4 @@
 <?php
     exec('git reset --hard HEAD && git pull');
-    exec('git clone https://github.com/leralins/Mr.REA && mv Mr.REA/build mr.reu/ui && git rm -r Mr.REA');
+    exec('git clone https://github.com/leralins/Mr.REA.git && cp Mr.REA/build/*.html ui && cp -R Mr.REA/build/ui/* ui && rm -rf Mr.REA');
  
